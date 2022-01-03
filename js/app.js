c386862a6681048b1f2045ff1fbb4aca6139337b
dantasyisland <dantothelastdrop@yahoo.com>
2022-01-03 00:28:21 -0600

    Have some things running modal next

--- js/app.js
+++ js/app.js
@@ -1,29 +1,65 @@
 function fetchData(url) {
   return fetch(url)
-    .then(res => res.json())
-    .then(res => iterate(res.results))
-  .then(res => console.log(res)) // because of return results
+    .then((res) => res.json())
+    .then((res) => iterate(res.results))
+    .then((res) => console.log(res)); // because of return results
 }
 
-
-
-fetchData('https://randomuser.me/api/?results=12')
+fetchData("https://randomuser.me/api/?results=12");
 
 function iterate(results) {
-  results.forEach(element => {
+  results.forEach((element) => {
+    const cardHTML = `<div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="${element.picture.medium}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${element.name.first} ${element.name.last}</h3>
+                        <p class="card-text">${element.email}</p>
+                        <p class="card-text cap">${element.location.city} ${element.location.state}</p>
+                    </div>
+                </div>`;
 
-    //For the card
-    //card-img-container
-    console.log(element.picture.thumbnail);
-
-    // card-info-container
-    console.log(element.name.first);
-    console.log(element.name.last);
-    console.log(element.email);
-    console.log(element.location.city);
-    console.log(element.location.state);
-
+    gallery.insertAdjacentHTML("beforeend", cardHTML);
   });
-  return results
+  return results;
 }
 
+const gallery = document.getElementById("gallery");
+gallery.addEventListener("click", (e) => {
+  if ((e.target.parentNode.nodeName) === 'DIV') {
+      console.log(e.target);
+  }; //
+
+});
+
+
+
+
+
+
+
+
+
+/**
+ * Modal Code - build this function and have fetch pass it for now - get the button working - gotta get one out of here
+ */
+
+function createModal(results) {
+
+  const modalHTML = `<div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+        </div>`
+
+}
---
+++ js/exampleobject.js
@@ -0,0 +1,56 @@
+{
+  "gender": "male",
+  "name": {
+    "title": "Monsieur",
+    "first": "Maxim",
+    "last": "Fabre"
+  },
+  "location": {
+    "street": {
+      "number": 7975,
+      "name": "Rue de L'Abbé-Roger-Derry"
+    },
+    "city": "Biglen",
+    "state": "Appenzell Ausserrhoden",
+    "country": "Switzerland",
+    "postcode": 3413,
+    "coordinates": {
+      "latitude": "69.5941",
+      "longitude": "-179.7482"
+    },
+    "timezone": {
+      "offset": "+5:00",
+      "description": "Ekaterinburg, Islamabad, Karachi, Tashkent"
+    }
+  },
+  "email": "maxim.fabre@example.com",
+  "login": {
+    "uuid": "6d89de34-20fe-4777-b4d8-aee8d32264e0",
+    "username": "greenduck590",
+    "password": "kathy",
+    "salt": "QYK72YY5",
+    "md5": "4b3bc4771c7e52b268bd6266d5b99b06",
+    "sha1": "a8e3e57034c43eb18e5ba9e7dcd93d2a805e2bec",
+    "sha256": "348d6c31b0af3cf97dbbf24f9631e879fe99e3c4d3ca1cb13086522f2e47c1f5"
+  },
+  "dob": {
+    "date": "1969-07-12T23:03:48.106Z",
+    "age": 53
+  },
+  "registered": {
+    "date": "2016-10-25T05:25:55.740Z",
+    "age": 6
+  },
+  "phone": "079 370 35 94",
+  "cell": "075 685 67 90",
+  "id": {
+    "name": "AVS",
+    "value": "756.3674.1999.79"
+  },
+  "picture": {
+    "large": "https://randomuser.me/api/portraits/men/88.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/88.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/88.jpg"
+  },
+  "nat": "CH"
+}
