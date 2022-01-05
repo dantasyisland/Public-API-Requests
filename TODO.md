4e695e8993dac9c926cc4c27052ed8181fdb9151
dantasyisland <dantothelastdrop@yahoo.com>
2022-01-05 01:19:27 -0600

    more changes - cleaner approach

---
+++ TODO.md
@@ -0,0 +1,25 @@
+# Public API Requests - Team Treehouse Project 5 🦄
+
+This app displays twelve users generated from the random user api (put link here). Clicking on a user card will display a modal of that user with more information. **ADD MORE**
+
+### To Do
+
+- [ ] Modal Display
+
+  - [ ] Toggle classname
+  - [ ] Styling
+  - [ ]
+  - [ ]
+
+- [ ] Formatting
+
+  - [ ] Review what needs to formatted add to this list
+  - [ ] **There will be more** Don't sleep on it kid
+
+- [ ] Search Functionality
+
+- [ ] README.md
+
+### Completed Column ✓
+
+- [x] Completed task title
--- js/app.js
+++ js/app.js
@@ -1,61 +1,91 @@
 function fetchData(url) {
   return fetch(url)
     .then((res) => res.json())
-    .then((res) => createCards(res.results)) // results returned -- have it return cards
-    .then((res) => createModals(res))
-    .then((res) => printArray(res))
-    .then((res) => addCards(res))
-    .then();
+    .then((res) => {
+      createCards(res);
+      createModals(res);
+    }).then((res) => getCards(res))
+    .then((res) => addEventListenerToCards(res))
+  // .then(() => {})
+  // .then(() => console.log('MORE TO DO'));
   // because of return results
 }
 
-fetchData("https://randomuser.me/api/?results=12");
+/**
+ *
+ */
 
-function createCards(results) {
-  results.forEach((element) => {
+const url = 'https://randomuser.me/api/?results=12';
+
+/**
+ * MIGHT NEED BODY BUT WE'LL SEE
+ */
+const body = document.body;
+const gallery = document.getElementById('gallery');
+
+const modalContainer = document.createElement('div');
+modalContainer.className = 'modal-container';
+gallery.insertAdjacentElement('afterend', modalContainer)
+
+
+fetchData(url);
+
+function createCards(result) {
+  result.results.forEach((element) => {
+    console.log(element);
     const cardHTML = `
       <div class="card">
         <div class="card-img-container">
           <img class="card-img" src="${element.picture.medium}" alt="profile picture">
         </div>
         <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${element.name.first} ${element.name.last}</h3>
+          <h3 id="name" class="card-name cap">${element.name.first} ${element.name.last} ${element.phone}</h3>
           <p class="card-text">${element.email}</p>
           <p class="card-text cap">${element.location.city} ${element.location.state}</p>
         </div>
       </div>`;
     gallery.insertAdjacentHTML("beforeend", cardHTML);
   });
-  return results;
+  return result;
 }
 
-function createModals(results) {
-  results.forEach((element) => {
+/**
+ * Modal Code - build this function and have fetch pass it for now - get the button working - gotta get one out of here
+ */
+
+function createModals(result) {
+  result.results.forEach((element) => {
     const modalHTML = `
-      <div class="modal-container">
-      <div class="modal">
-          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-          <div class="modal-info-container">
-              <img class="modal-img" src="${element.picture.medium}" alt="profile picture">
-              <h3 id="name" class="modal-name cap">${element.name.first} ${element.name.last}</h3>
-              <p class="modal-text">${element.email}</p>
-              <p class="modal-text cap">${element.location.city} ${element.location.state}</p>
-              <hr>
-              <p class="modal-text">(555) 555-5555</p>
-              <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-              <p class="modal-text">Birthday: 10/21/2015</p>
-          </div>
-      </div>`;
-    gallery.insertAdjacentHTML("beforeend", modalHTML);
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${element.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${element.name.first} ${element.name.last}</h3>
+            <p class="modal-text">${element.email}</p>
+            <p class="modal-text cap">${element.location.city} ${element.location.state}</p>
+            <hr>
+            <p class="modal-text">${element.phone}</p>
+            <p class="modal-text">${element.location.street.number} ${element.location.street.name} ${element.location.city}
+                ${element.location.state} ${element.location.postcode} ${element.location.country}</p>
+            <p class="modal-text">${element.dob.date} </p>
+        </div>
+    </div>`
+    modalContainer.insertAdjacentHTML("beforeend", modalHTML);
+    return result;
   });
-  return results;
 }
 
-// Gotta get the name out
+function getCards(results) {
+  // console.log(results);
+  const cards = document.getElementById("gallery").children;
+  return cards;
+}
 
-
-// create add event listener - pass func?
-function addCards(cards) {
+/**
+ * Adds Event Listeners To Cards
+ * @param {HTMLCollection} cards
+ */
+function addEventListenerToCards(cards) {
   for (let i = 0; i < cards.length; i++) {
     // console.log(cards[i]);
     cards[i].addEventListener("click", (e) => {
@@ -64,43 +94,9 @@ function addCards(cards) {
   }
 }
 
-function printArray(results) {
-  // console.log(results);
-  const cards = document.getElementById("gallery").children;
-  return cards;
-}
-
 // iterate through modal - match email to email
 
 function kickOutWhatINeed(card) {
-  console.log(card);
-}
-
-// gallery.addEventListener("click", (e) => {
-//   if ((e.target.parentNode.nodeName) === 'DIV') {
-//     console.log(e.target.parentNode);
-
-//   }; //
-
-// });
-
-/**
- * Modal Code - build this function and have fetch pass it for now - get the button working - gotta get one out of here
- */
-
-function createModal(results) {
-  const modalHTML = `<div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
-                        <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
-                    </div>
-        </div>`;
+  const chosenCard = card.children[1].children[1].innerText; // Gives email
+  console.log(chosenCard);
 }
