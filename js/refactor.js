6406af65fe78ecdfaa0905c560a29cd625173872
dantasyisland <dantothelastdrop@yahoo.com>
2022-01-08 00:51:15 -0600

    refactoring

--- js/refactor.js
+++ js/refactor.js
@@ -1,198 +1,144 @@
-let fetchresults;
+const url = "https://randomuser.me/api/?results=12";
+const gallery = document.getElementById('gallery');
+let employees = [];
 
+
+fetchData(url);
 
 function fetchData(url) {
-  return fetch(url)
-    .then((result) => result.json())
-    .then((result) => createCards(result))
-    .then((result) => fetchresults = result)
-  //   createModals(result);
-  // })
-  // .then((result) => getCards(result))
-  // .then((result) => addEventListenerToCards(result))
-  // .then(() => addEventListenerToModalButtons())
-  // .then(() => console.log("done"))
-  // .catch(error => console.log(error));
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => createEmployeeArray(res))
+    .then((res) => createCards(res.results))
+    .catch((error) => console.log(error))
+}
 
-  // .then(() => {})
-  // .then(() => console.log('MORE TO DO'));
-  // because of return results
+function createEmployeeArray(res) {
+  employees = res.results;
+  return res
+}
+
+function createCards(array) {
+  array.forEach((employee, index) => {
+    createCard(employee, index);
+  });
+};
+
+function createCard(employee, index) {
+  const cardHTML = `
+      <div class="card" data-index-number = "${index}">
+        <div class="card-img-container">
+          <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+          <p class="card-text">${employee.email}</p>
+          <p class="card-text cap">${employee.location.city} ${employee.location.state}</p>
+        </div>
+      </div>`;
+  gallery.insertAdjacentHTML("beforeend", cardHTML);
+
+  cards = gallery.children;
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", (e) => {
+      showModal(employees, cards[i].dataset.indexNumber);
+    });
+  }
+};
+
+
+function showModal(userArray, index) {
+
+  createModal(userArray, index);
+
+  const modal = document.querySelector(`[data-modal-index-number = '${index}']`);
+  const closeButton = modal.querySelector(".modal-close-btn");
+  const modalContainer = modal.parentElement;
+
+  modalContainer.style.display = 'block';
+
+  closeButton.addEventListener("click", (e) => {
+    modalContainer.style.display = 'none';
+    modalContainer.remove();
+  });
+
+  const prevButton = document.getElementById('modal-prev');
+  const nextButton = document.getElementById('modal-next');
+
+  prevButton.addEventListener('click', (e) => {
+    modalContainer.remove();
+    index--;
+    if (index < 0) index = employees.length - 1;
+    showModal(employees, index);
+  })
+
+  nextButton.addEventListener('click', (e) => {
+    modalContainer.remove();
+    index++;
+    if (index > employees.length - 1) index = 0;
+    showModal(employees, index);
+  })
+
 }
 
 
+function createModal(userArray, index) {
 
+  let employee = userArray[index];
+  console.log(userArray[index]);
+  dob = new Date(employee.dob.date).toLocaleDateString();
 
-
-
-/**
- * Rewriting this
- * Make a list of what's called - try a second script and just call inside event listeners - don't need to create a modal for each one - just the one being called
- */
-
-
-
-
-
-
-
-
-
-// Found in both the card and modal window - selects the paragraph that contains the email
-const firstParagraph = "p:first-of-type";
-/**
- * Modal Code - build this function and have fetch pass it for now - get the button working - gotta get one out of here
- */
-
-/**
- *
- * @param {*} result
- */
-
-function createModals(result) {
-  let index = 0;
-
-  result.results.forEach((element) => {
-    dob = new Date(element.dob.date).toLocaleDateString();
-
-    const modalHTML = `
-    <div class="modal" data-index-number = "${index}">
+  const modalHTML = `
+  <div class="modal-container">
+    <div class="modal" data-modal-index-number = "${index}">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container">
-            <img class="modal-img" src="${element.picture.large}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${element.name.first} ${element.name.last}</h3>
-            <p class="modal-text">${element.email}</p>
-            <p class="modal-text cap">${element.location.city} ${element.location.state}</p>
+            <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <p class="modal-text">${employee.email}</p>
+            <p class="modal-text cap">${employee.location.city} ${employee.location.state}</p>
             <hr>
-            <p class="modal-text">${element.phone}</p>
-            <p class="modal-text">${element.location.street.number} ${element.location.street.name} ${element.location.city}
-                ${element.location.state} ${element.location.postcode} ${element.location.country}</p>
+            <p class="modal-text">${employee.phone}</p>
+            <p class="modal-text">${employee.location.street.number} ${employee.location.street.name} ${employee.location.city}
+                ${employee.location.state} ${employee.location.postcode} ${employee.location.country}</p>
             <p class="modal-text">${dob} </p>
         </div>
+        <div class="modal-btn-container">
+    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
     </div>`;
-    modalContainer.insertAdjacentHTML("beforeend", modalHTML);
-    index++;
-  });
-
-  const modalButtonHTML = `<div class="modal-btn-container">
-  <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-  <button type="button" id="modal-next" class="modal-next btn">Next</button>
-  </div>`;
-
-  modalContainer.insertAdjacentHTML("beforeend", modalButtonHTML);
-  getCards(result);
+  gallery.insertAdjacentHTML("afterend", modalHTML);
 }
 
-/**
- *
- * @param {*} results
- * @returns
- */
 
-function getCards(results) {
-  // console.log(results);
-  const cards = document.getElementById("gallery").children;
-  addEventListenerToCards(cards);
-}
+const searchContainer = document.querySelector('.search-container');
+const searchHTML = `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`;
 
-/**
- * Adds Event Listeners To Cards
- * @param {HTMLCollection} cards
- */
-function addEventListenerToCards(cards) {
-  for (let i = 0; i < cards.length; i++) {
-    // console.log(cards[i]);
-    cards[i].addEventListener("click", (e) => {
-      kickOutWhatINeed(cards[i]);
-    });
+searchContainer.insertAdjacentHTML('beforeend', searchHTML);
+
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('keyup', (e) => {
+  if (e.target.value === '') {
+    createCards(employees);
   }
-}
+  search(e.target.value);
+})
 
-/**
- *
- * @param {HTMLElement} card
- */
-function kickOutWhatINeed(card) {
-  const chosenCardEmail = card.querySelector(firstParagraph).textContent; // Gives email
-  showModal(chosenCardEmail);
-}
 
-/**
- *
- * @param {*} chosenCard
- */
+function search(input) {
 
-function showModal(chosenCardEmail) {
-  const email = chosenCardEmail;
-  const collection = document.querySelectorAll(".modal");
-  const modalContainer = document.querySelector(".modal-container");
-
-  collection.forEach((modal) => {
-    const modalEmail = modal.querySelector("p:first-of-type").innerText;
-    if (email === modalEmail) {
-      const closeButton = modal.querySelector(".modal-close-btn");
-      modalContainer.style.display = "block";
-      modal.style.display = "block";
-      modal.classList.add("active");
-      closeButton.addEventListener("click", (e) => {
-        modal.style.display = "none";
-        modalContainer.style.display = "none";
-        modal.classList.remove("active");
-      });
+  let newCards = [];
+  for (let i = 0; i <= employees.length - 1; i++) {
+    if (input.length != 0 && (employees[i].name.first.toLowerCase().includes(input.toLowerCase())) || employees[i].name.last.toLowerCase().includes(input.toLowerCase())) {
+      newCards.push(employees[i]);
     }
-  });
-}
-
-/**
- *
- * @param {*} index
- */
-function ChangeModal(index) {
-  const modalLength = document.querySelectorAll(".modal").length;
-  const collection = document.querySelectorAll(".modal");
-  if (index < 0) {
-    index = modalLength - 1;
-  } else if (index > modalLength - 1) {
-    index = 0;
-  } else {
-    console.log(index);
   }
-  console.log(index);
-  collection.forEach((modal) => {
-    const closeButton = modal.querySelector(".modal-close-btn");
-    closeButton.addEventListener("click", (e) => {
-      modal.style.display = "none";
-      modalContainer.style.display = "none";
-      modal.classList.remove("active");
-    });
-
-    if (parseInt(modal.dataset.indexNumber) === index) {
-      modal.classList.add("active");
-      modal.style.display = "block";
-      console.log("yeah");
-    }
-  });
-}
-
-/**
- * What was I doing last?
- * showModal fucntion - takes card email and checks in first of type
- */
-
-function addEventListenerToModalButtons() {
-  const previousButton = document.getElementById("modal-prev");
-  const nextButton = document.getElementById("modal-next");
-  previousButton.addEventListener("click", (e) => {
-    const activeModalCard = document.querySelector(".active");
-    const activeModalCardIndex = parseInt(activeModalCard.dataset.indexNumber);
-    activeModalCard.classList.remove("active");
-    activeModalCard.style.display = "none";
-    ChangeModal(activeModalCardIndex - 1);
-  });
-  nextButton.addEventListener("click", (e) => {
-    const activeModalCard = document.querySelector(".active");
-    const activeModalCardIndex = parseInt(activeModalCard.dataset.indexNumber);
-    activeModalCard.classList.remove("active");
-    activeModalCard.style.display = "none";
-    ChangeModal(activeModalCardIndex + 1);
-  });
+  gallery.innerHTML = '';
+  createCards(newCards);
+  console.log(newCards[1]);
 }
--- js/refactorscratch.js
+++ js/refactorscratch.js
@@ -1,52 +1,144 @@
 const url = "https://randomuser.me/api/?results=12";
 const gallery = document.getElementById('gallery');
 let employees = [];
+let employeeArray = [];
 
-//CREATE HTML FOR THIS
-const modalContainer = document.createElement("div");
-modalContainer.className = "modal-container";
-gallery.insertAdjacentElement("afterend", modalContainer);
 
 fetchData(url);
-
 
 function fetchData(url) {
   fetch(url)
     .then((res) => res.json())
+    .then((res) => (createUserArray(res)))
     .then((res) => createCards(res.results))
     .catch((error) => console.log(error))
 }
 
+function createUserArray(res) {
+  employeeArray = res.results;
+  return res
+}
+
+
 function createCards(results) {
-  let index = 0;
   employees = results;
-  results.forEach((element, index) => {
-    const cardHTML = `
+  employees.forEach((employee, index) => {
+    createCard(employee, index);
+  });
+};
+
+function createCard(employee, index) {
+  const cardHTML = `
       <div class="card" data-index-number = "${index}">
         <div class="card-img-container">
-          <img class="card-img" src="${element.picture.medium}" alt="profile picture">
+          <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
         </div>
         <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${element.name.first} ${element.name.last}</h3>
-          <p class="card-text">${element.email}</p>
-          <p class="card-text cap">${element.location.city} ${element.location.state}</p>
+          <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+          <p class="card-text">${employee.email}</p>
+          <p class="card-text cap">${employee.location.city} ${employee.location.state}</p>
         </div>
       </div>`;
-    gallery.insertAdjacentHTML("beforeend", cardHTML);
-  });
+  gallery.insertAdjacentHTML("beforeend", cardHTML);
 
   cards = gallery.children;
   for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", (e) => {
-      showModal(cards[i], cards[i].dataset.indexNumber);
+      showModal(employee, cards[i].dataset.indexNumber);
     });
   }
-
-
 };
 
 
-function showModal(card, index) {
-  console.log(card);
-  console.log(index);
+function showModal(employees, index) {
+
+  createModal(employees, index);
+
+  const modal = document.querySelector(`[data-modal-index-number = '${index}']`);
+  const closeButton = modal.querySelector(".modal-close-btn");
+  const modalContainer = modal.parentElement;
+
+  modalContainer.style.display = 'block';
+
+  closeButton.addEventListener("click", (e) => {
+    modalContainer.style.display = 'none';
+    modalContainer.remove();
+  });
+
+  const prevButton = document.getElementById('modal-prev');
+  const nextButton = document.getElementById('modal-next');
+
+  prevButton.addEventListener('click', (e) => {
+    modalContainer.remove();
+    index--;
+    if (index < 0) index = employees.length - 1;
+    showModal(employees, index);
+  })
+
+  nextButton.addEventListener('click', (e) => {
+    modalContainer.remove();
+    index++;
+    if (index > employees.length - 1) index = 0;
+    showModal(employees, index);
+  })
+
+}
+
+
+function createModal(employees, index) {
+
+  let employee = employees[index]
+  dob = new Date(employee.dob.date).toLocaleDateString();
+
+  const modalHTML = `
+  <div class="modal-container">
+    <div class="modal" data-modal-index-number = "${index}">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <p class="modal-text">${employee.email}</p>
+            <p class="modal-text cap">${employee.location.city} ${employee.location.state}</p>
+            <hr>
+            <p class="modal-text">${employee.phone}</p>
+            <p class="modal-text">${employee.location.street.number} ${employee.location.street.name} ${employee.location.city}
+                ${employee.location.state} ${employee.location.postcode} ${employee.location.country}</p>
+            <p class="modal-text">${dob} </p>
+        </div>
+        <div class="modal-btn-container">
+    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+    </div>`;
+  gallery.insertAdjacentHTML("afterend", modalHTML);
+}
+
+
+const searchContainer = document.querySelector('.search-container');
+const searchHTML = `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`;
+
+searchContainer.insertAdjacentHTML('beforeend', searchHTML);
+
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('keyup', (e) => {
+  search(e.target.value);
+})
+
+
+function search(input) {
+  gallery.innerHTML = '';
+  if (input === '') {
+    createCards(employeeArray)
+  }
+  let newCards = [];
+  for (let i = 0; i <= employees.length - 1; i++) {
+    if (input.length != 0 && (employees[i].name.first.toLowerCase().includes(input.toLowerCase())) || employees[i].name.last.toLowerCase().includes(input.toLowerCase())) {
+      newCards.push(employees[i]);
+    }
+  }
+  createCards(newCards)
 }
